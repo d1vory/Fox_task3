0134e11d7b6059bb06de2c01e2fcb02ae17109ac
Olexandr <dubchakob@gmail.com>
2024-02-27 17:46:35 +0200

    added user input

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -4,14 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        const string filename = @"C:\Users\Olexandr\MyProjects\CSHARP\foxminded\task3\Task3\files\numbers.txt";
-        var parser = new FileParser(filename);
-        Console.WriteLine($"Max sum line is {parser.GetLineWithMaximumSum()}");
-
-        foreach (int i in parser.GetBrokenLinesIndexes())
+        string filename;
+        if (args.Length == 0)
         {
-            Console.Write($"{i}, ");
+            Console.WriteLine("Enter a path to file: ");
+            filename = Console.ReadLine().Trim();
         }
-        
+        else
+        {
+            filename = args[0];
+        }
+
+        try
+        {
+            var parser = new FileParser(filename);
+
+            Console.WriteLine($"Line with maximal sum of elements is {parser.GetLineWithMaximumSum()}");
+
+            var brokenLinesIndexes = parser.GetBrokenLinesIndexes();
+
+            Console.Write("Broken lines are: [");
+            foreach (int i in parser.GetBrokenLinesIndexes())
+            {
+                Console.Write($"{i}, ");
+            }
+            Console.Write("]\n");
+
+        }
+        catch (IOException ex)
+        {
+            Console.WriteLine("Given path is not valid!");
+            return;
+        }
+
     }
 }
